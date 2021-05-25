93e0e644611cdb79e1e09a44ee786f58750b1e74
Candace <candacelee5206@e.newera.edu.my>
2021-05-25 10:52:37 +0800

    variable created

---
+++ week2/exercise1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    echo "Candace Lee Xiao Xuen 1930165";
+    ?>
+
+</body>
+
+</html>
---
+++ week2/exercise2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$color = "red";
+$number = "2";
+$name = "candace";
+echo "My favourite colour is " . $color . "<br>";
+echo "My favourite number is " . $number . "<br>";
+echo "My name is " . $name . "<br>";
+?>
+</body>
+</html>
