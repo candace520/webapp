3143a4d2843b7766c9e60c790bc9c21846f1fdec
Candace <candacelee5206@e.newera.edu.my>
2021-05-26 20:29:19 +0800

    dropdown menu with array option

--- week2/homework1.php
+++ week2/homework1.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -9,49 +10,51 @@
 <style>
 
 </style>
+
 <body>
 
-<div class="container">                                         
-  <div class="btn-group">
-    <button class="btn btn-info btn-lg dropdown-toggle" type="button" data-toggle="dropdown">Day
-    <span class="caret"></span></button>
-    
-    <ul class="dropdown-menu">
-      <?php
+  <div class="container">
+    <div class="btn-group">
+      <button class="btn btn-info btn-lg dropdown-toggle" type="button" data-toggle="dropdown">Day
+        <span class="caret"></span></button>
+
+      <ul class="dropdown-menu">
+        <?php
         for ($i = 1; $i <= 31; $i++) {
-     echo $i."<br>";
-      } 
-      ?>
-      
-    </ul>
-  </div>
-  <div class="btn-group">
-    <button class="btn btn-warning btn-lg dropdown-toggle" type="button" data-toggle="dropdown">Month
-    <span class="caret"></span></button>
-    
-    <ul class="dropdown-menu">
-      <?php
+          echo $i . "<br>";
+        }
+        ?>
+
+      </ul>
+    </div>
+    <div class="btn-group">
+      <button class="btn btn-warning btn-lg dropdown-toggle" type="button" data-toggle="dropdown">Month
+        <span class="caret"></span></button>
+
+      <ul class="dropdown-menu">
+        <?php
         for ($i = 1; $i <= 12; $i++) {
-     echo $i."<br>";
-      } 
-      ?>
-      
-    </ul>
-  </div>
-  <div class="btn-group">
-    <button class="btn btn-danger btn-lg dropdown-toggle" type="button" data-toggle="dropdown">Year
-    <span class="caret"></span></button>
-    
-    <ul class="dropdown-menu">
-      <?php
+          echo $i . "<br>";
+        }
+        ?>
+
+      </ul>
+    </div>
+    <div class="btn-group">
+      <button class="btn btn-danger btn-lg dropdown-toggle" type="button" data-toggle="dropdown">Year
+        <span class="caret"></span></button>
+
+      <ul class="dropdown-menu">
+        <?php
         for ($i = 1900; $i <= 2021; $i++) {
-     echo $i."<br>";
-      } 
-      ?>
-      
-    </ul>
+          echo $i . "<br>";
+        }
+        ?>
+
+      </ul>
+    </div>
   </div>
-</div>
 
 </body>
+
 </html>
