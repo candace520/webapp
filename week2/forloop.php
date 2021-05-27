46f81dd33439856dcc1e0a78b8a6158d2c63cc40
candace <candacelee5206@e.newera.edu.my>
2021-05-27 11:11:54 +0800

    create for loop of php

--- week2/forloop.php
+++ week2/forloop.php
@@ -2,15 +2,15 @@
 <html>
 
 <head>
-     <title>for loop of php
-     </title>
+     <title>for loop of php</title>
 </head>
 
 <body>
      <?php
      for ($i = 0; $i < 10; $i++) {
-          echo $i. "<br>";
+          echo $i . "<br>";
      }
      ?>
 </body>
+
 </html>
