30372a38ff6de596a3a9918bd4d4e1a25d6a010a
candace <candacelee5206@e.newera.edu.my>
2021-05-27 10:25:50 +0800

    create variable on php

--- week2/exercise2.php
+++ week2/exercise2.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>exercise2</title>
+    <title>exercise2</title>
 </head>
+
 <body>
-<?php
-$color = "red";
-$number = 2;
-$name = "candace";
-echo "My favourite colour is " . $color . "<br>";
-echo "My favourite number is " . $number . "<br>";
-echo "My name is " . $name . "<br>";
-?>
+    <?php
+    $color = "red";
+    $number = 2;
+    $name = "candace";
+    echo "My favourite colour is " . $color . "<br>";
+    echo "My favourite number is " . $number . "<br>";
+    echo "My name is " . $name . "<br>";
+    ?>
 </body>
+
 </html>
