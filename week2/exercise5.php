f5c68fa387ef1b691be944a7908db3d4da8eb369
candace <candacelee5206@e.newera.edu.my>
2021-05-27 10:28:28 +0800

    find larger number with compare

--- week2/exercise5.php
+++ week2/exercise5.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>exercise5</title>
+  <title>exercise5</title>
 </head>
+
 <body>
-<?php
-$x = rand(1,100);
-$y = rand(1,100);
-$total =$x + $y ;
-echo "$x * $y =" . $x * $y. "<br>";
-echo "$x + $y = " .$total. "<br>";
-if ($x > $y) { 
-  echo "The larger random number is " . $x;
-}
-?>
+  <?php
+  $x = rand(1, 100);
+  $y = rand(1, 100);
+  $total = $x + $y;
+  echo "$x * $y =" . $x * $y . "<br>";
+  echo "$x + $y = " . $total . "<br>";
+  if ($x > $y) {
+    echo "The larger random number is " . $x;
+  }
+  ?>
 
 </body>
+
 </html>
