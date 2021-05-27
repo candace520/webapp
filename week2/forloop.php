99a7694f5c933cd51351c48b8b9983a95d9b0aae
candace <candacelee5206@e.newera.edu.my>
2021-05-27 11:11:02 +0800

    create dropdown menu with for loop

--- week2/forloop.php
+++ week2/forloop.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>for loop of php</title>
+     <title>for loop of php
+     </title>
 </head>
+
 <body>
-
-
      <?php
      for ($i = 0; $i < 10; $i++) {
-          echo $i . "<br>";
+          echo $i. "<br>";
      }
      ?>
-
 </body>
-
 </html>
