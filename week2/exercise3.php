307131148915498190c6a4b2cfac3462f968e509
candace <candacelee5206@e.newera.edu.my>
2021-05-27 10:27:14 +0800

    operation of math

--- week2/exercise3.php
+++ week2/exercise3.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>exercise3</title>
+    <title>exercise3</title>
 </head>
+
 <body>
-<?php
-$x = 2;
-$y = 6;
-$total =$x + $y ;
-echo "$x * $y =" . $x * $y. "<br>";
-echo "$x + $y = " .$total. "<br>";
-?>
+    <?php
+    $x = 2;
+    $y = 6;
+    $total = $x + $y;
+    echo "$x * $y =" . $x * $y . "<br>";
+    echo "$x + $y = " . $total . "<br>";
+    ?>
 </body>
+
 </html>
