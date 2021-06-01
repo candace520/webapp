aeda5ea7365f69ebb82ad7dfef5cc63d6ff5c9fd
candace <candacelee5206@e.newera.edu.my>
2021-06-01 12:59:02 +0800

    create form to database

---
+++ online_store/config/database.php
@@ -0,0 +1,16 @@
+<?php
+// used to connect to the database
+$host = "localhost";
+$db_name = "candace_store";
+$username = "candace_store";
+$password = "07240206xuen";
+  
+try {
+    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+    echo "Connected successfully"; 
+}  
+// show error
+catch(PDOException $exception){
+    echo "Connection error: ".$exception->getMessage();
+}
+?>
