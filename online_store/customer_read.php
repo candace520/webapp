5b1d4673b10c1954a5a3675c9efe54ad26e8760d
Candace <candacelee5206@e.newera.edu.my>
2021-06-15 18:03:18 +0800

    read the details of all product wit its id

---
+++ online_store/customer_read.php
@@ -0,0 +1,96 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <!-- container -->
+    <div class="container">
+        <div class="page-header">
+            <h1>Read Customers</h1>
+        </div>
+
+        <!-- PHP code to read records will be here -->
+        <?php
+        // include database connection
+        include 'config/database.php';
+
+        // delete message prompt will be here
+
+        // select all data
+        $query = "SELECT * FROM customer ORDER BY id DESC";
+        $stmt = $con->prepare($query);
+        $stmt->execute();
+
+        // this is how to get number of rows returned
+        //this is how to get how many product found
+        $num = $stmt->rowCount();
+
+        // link to create record form
+        echo "<a href='customer.php' class='btn btn-primary m-b-1em'>Create New Customers</a>";
+
+        //check if more than 0 record found
+        if ($num > 0) {
+
+            // data from database will be here
+            echo "<table class='table table-hover table-responsive table-bordered'>"; //start table
+
+            //creating our table heading
+            echo "<tr>";
+            echo "<th>ID</th>";
+            echo "<th>User Name</th>";
+            echo "<th>Gender</th>";
+            echo "<th>Date of birth</th>";
+            echo "<th>Action</th>";
+            echo "</tr>";
+
+            // table body will be here
+            // retrieve our table contents
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                // extract row
+                // this will make $row['firstname'] to just $firstname only
+                extract($row);
+                // creating new table row per record
+                echo "<tr>";
+                echo "<td>{$id}</td>";
+                echo "<td>{$name}</td>";
+                echo "<td>{$gender}</td>";
+                echo "<td>{$dateofbirth}</td>";
+                echo "<td>";
+                // read one record
+                echo "<a href='customer_read_one.php?id={$id}' class='btn btn-info m-r-1em'>Read</a>";
+
+                // we will use this links on next part of this post
+                echo "<a href='product_update.php?id={$id}' class='btn btn-primary m-r-1em'>Edit</a>";
+
+                // we will use this links on next part of this post
+                echo "<a href='#' onclick='delete_user({$id});'  class='btn btn-danger'>Delete</a>";
+                echo "</td>";
+                echo "</tr>";
+            }
+
+
+
+            // end table
+            echo "</table>";
+        }
+
+        // if no records found
+        else {
+            echo "<div class='alert alert-danger'>No records found.</div>";
+        }
+        ?>
+
+
+    </div> <!-- end .container -->
+
+    <!-- confirm delete record will be here -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</body>
+
+</html>
