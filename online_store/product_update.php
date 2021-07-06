f4a14b43356194109bf3cc55eeeeb4505da2bee5
Candace <candacelee5206@e.newera.edu.my>
2021-07-06 13:42:07 +0800

    updating product page is created

--- online_store/product_update.php
+++ online_store/product_update.php
@@ -8,166 +8,137 @@ if (!isset($_SESSION["cus_username"])) {
 <html>
 
 <head>
-    <title>PDO - Read Records - PHP CRUD Tutorial</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <style>
-        .m-r-1em {
-            margin-right: 1em;
-        }
-
-        .m-b-1em {
-            margin-bottom: 1em;
-        }
-
-        .m-l-1em {
-            margin-left: 1em;
-        }
-
-        .mt0 {
-            margin-top: 0;
-        }
-    </style>
+    <title>Homework - Update Product</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 </head>
 
 <body>
-    <?php
-    include 'menu.php';
-    ?>
+<?php
+        include 'menu.php';
+        ?>
     <div class="container">
+        
         <div class="page-header">
             <h1>Update Product</h1>
         </div>
-
-
         <?php
-        //PHP read record by ID will be here 
+        $productID = isset($_GET['productID']) ? $_GET['productID'] : die('ERROR: Product record not found.');
 
-        // get passed parameter value, in this case, the record ID
-        // isset() is a PHP function used to verify if a value is there or not
-        $productID = isset($_GET['productID']) ? $_GET['productID'] : die('ERROR: Record ID not found.');
-
-        //include database connection
         include 'config/database.php';
-
-        // read current record's data
         try {
-
-            // prepare select query
-            $query = "SELECT productID, name, description, price FROM products WHERE productID = ? LIMIT 0,1";
+            $query = "SELECT * FROM products WHERE productID = :productID ";
             $stmt = $con->prepare($query);
-
-            // this is the first question mark
-            $stmt->bindParam(1, $productID);
-
-            // execute our query
+            $stmt->bindParam(":productID", $productID);
             $stmt->execute();
-
-            // store retrieved row to a variable
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            // values to fill up our form
+            $productID = $row['productID'];
             $name = $row['name'];
+            $nameMalay = $row['nameMalay'];
             $description = $row['description'];
             $price = $row['price'];
-        }
-
-        // show error
-        catch (PDOException $exception) {
+            $promotion_price = $row['promotion_price'];
+            $manufacture_date = $row['manufacture_date'];
+            $expired_date = $row['expired_date'];
+        } catch (PDOException $exception) {
             die('ERROR: ' . $exception->getMessage());
         }
 
-        ?>
-
-        <?php
-
-        //PHP post to update record will be here
-        // check if form was submitted
         if ($_POST) {
             try {
-                if (
-                    empty($_POST['name'])  
-                    || empty($_POST['description']) || empty($_POST['price'])
-                ) {
-                    throw new Exception("Please make sure all fields are not empty");
+                if (empty($_POST['name']) || empty($_POST['description']) || empty($_POST['price']) || empty($_POST['manufacture_date']) || empty($_POST['expired_date'])) {
+                    throw new Exception("Make sure all fields are not empty");
                 }
-                if (!is_numeric($_POST['price'])) {
-                    throw new Exception(" <div class='alert alert-danger'>Please make sure the price is a number.</div>");
+                if (!is_numeric($_POST['price']) || !is_numeric($_POST['promotion_price'])) {
+                    throw new Exception("Please make sure the price is a number");
                 }
-                if ($_POST['price'] < 0) {
-                    throw new Exception("<div class='alert alert-danger'>Please make sure the price must be not a negative value.</div>");
+                if ($_POST['price'] < 0 || $_POST['promotion_price'] < 0) {
+                    throw new Exception("Please make sure the price must be not a negative value");
                 }
-
-                if ($_POST['price'] > 1000) {
-                    throw new Exception("<div class='alert alert-danger'>Please make sure the price must be not bigger than RM 1000.</div>");
+                if ($_POST['price'] > 1000 || $_POST['promotion_price'] > 1000) {
+                    throw new Exception("Please make sure the price is not bigger than RM 1000.");
                 }
-                // write update query
-                // in this case, it seemed like we have so many fields to pass and
-                // it is better to label them and not use question marks
-                $query = "UPDATE products
-                  SET name=:name, description=:description,
-   price=:price WHERE productID = :productID";
-                // prepare query for excecution
+                if ($_POST['price'] < $_POST['promotion_price']) {
+                    throw new Exception("Promotion price cannot bigger than normal price.");
+                }
+                if ($_POST['manufacture_date'] > $_POST['expired_date']) {
+                    throw new Exception("Please make sure expired date is late than the manufacture date.");
+                }
+                $query = "UPDATE products SET name=:name, nameMalay=:nameMalay, description=:description,
+                         price=:price, promotion_price=:promotion_price, manufacture_date=:manufacture_date, expired_date=:expired_date WHERE productID = :productID";
                 $stmt = $con->prepare($query);
-                // posted values
                 $name = htmlspecialchars(strip_tags($_POST['name']));
+                $nameMalay = htmlspecialchars(strip_tags($_POST['nameMalay']));
                 $description = htmlspecialchars(strip_tags($_POST['description']));
                 $price = htmlspecialchars(strip_tags($_POST['price']));
-                // bind the parameters
+                $promotion_price = htmlspecialchars(strip_tags($_POST['promotion_price']));
+                $manufacture_date = htmlspecialchars(strip_tags($_POST['manufacture_date']));
+                $expired_date = htmlspecialchars(strip_tags($_POST['expired_date']));
+                $stmt->bindParam(':productID', $productID);
                 $stmt->bindParam(':name', $name);
+                $stmt->bindParam(':name_malay', $name_malay);
                 $stmt->bindParam(':description', $description);
                 $stmt->bindParam(':price', $price);
-                $stmt->bindParam(':productID', $productID);
-                // Execute the query
+                $stmt->bindParam(':promotion_price', $promotion_price);
+                $stmt->bindParam(':manufacture_date', $manufacture_date);
+                $stmt->bindParam(':expired_date', $expired_date);
                 if ($stmt->execute()) {
                     echo "<div class='alert alert-success'>Record was updated.</div>";
                 } else {
                     echo "<div class='alert alert-danger'>Unable to update record. Please try again.</div>";
                 }
-            }
-            // show errors
-            catch (PDOException $exception) {
+            } catch (PDOException $exception) {
                 die('ERROR: ' . $exception->getMessage());
+            
             } catch (Exception $exception) {
-                echo "<div class='alert alert-danger'>" . $exception->getMessage() . "</div>";
+                echo "<div class='alert alert-danger'>".$exception->getMessage()."</div>";
             }
         } ?>
 
-
-        <!--we have our html form here where new record information can be updated-->
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?productID={$productID}"); ?>" method="post">
             <table class='table table-hover table-responsive table-bordered'>
                 <tr>
+                    <td>Product ID</td>
+                    <td><?php echo htmlspecialchars($productID, ENT_QUOTES);  ?></td>
+                </tr>
+                <tr>
                     <td>Name</td>
-                    <td><input type='text' name='name' value="<?php echo htmlspecialchars($name, ENT_QUOTES);  ?>" class='form-control' /></td>
+                    <td><input type='text' name='name' value="<?php echo htmlspecialchars($name, ENT_QUOTES); ?>" class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Malay Name</td>
+                    <td><input type='text' name='nameMalay' value="<?php echo htmlspecialchars($nameMalay, ENT_QUOTES); ?>" class='form-control' /></td>
                 </tr>
                 <tr>
                     <td>Description</td>
-                    <td><textarea name='description' class='form-control'><?php echo htmlspecialchars($description, ENT_QUOTES);  ?></textarea></td>
+                    <td><textarea name='description' class='form-control'><?php echo htmlspecialchars($description, ENT_QUOTES); ?></textarea></td>
                 </tr>
                 <tr>
                     <td>Price</td>
+                    <td><input type='text' name='price' value="<?php echo htmlspecialchars($price, ENT_QUOTES); ?>" class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Promotion Price</td>
+                    <td><input type='text' name='promotion_price' value="<?php echo htmlspecialchars($promotion_price, ENT_QUOTES); ?>" class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Manufacture Date</td>
+                    <td><input type='date' name='manufacture_date' value="<?php echo htmlspecialchars($manufacture_date, ENT_QUOTES);  ?>" class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Expired Date</td>
+                    <td><input type='date' name='expired_date' value="<?php echo htmlspecialchars($expired_date, ENT_QUOTES);  ?>" class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td></td>
                     <td>
-                        <div class="input-group">
-                            <span class="input-group-text">RM</span>
-                            <input type='text' name='price' value="<?php echo htmlspecialchars($price, ENT_QUOTES);  ?>" class='form-control' />
+                        <input type='submit' value='Save Changes' class='btn btn-primary' />
+                        <a href='product_read.php' class='btn btn-danger'>Back to read product</a>
                     </td>
                 </tr>
+            </table>
+        </form>
     </div>
-    <tr>
-        <td></td>
-        <td>
-            <input type='submit' value='Save Changes' class='btn btn-primary' />
-            <a href='product_read.php' class='btn btn-danger'>Back to read products</a>
-        </td>
-    </tr>
-    </table>
-    </form>
-
-    </div>
-    <!-- end .container -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 
 </html>
