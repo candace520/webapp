f5daab00789c0cc1eb6a29de78449f663e315107
candace520 <84360103+candace520@users.noreply.github.com>
2021-07-12 22:46:10 +0800

    Add files via upload

--- online_store/product_delete.php
+++ online_store/product_delete.php
@@ -7,7 +7,15 @@ try {
     // isset() is a PHP function used to verify if a value is there or not
     $productID = isset($_GET['productID']) ? $_GET['productID'] :  die('ERROR: Record ID not found.');
 
-     
+     // delete query
+     $proquery = "SELECT * FROM order_detail WHERE productID = ?";
+     $prostmt = $con->prepare($proquery);
+     $prostmt->bindParam(1, $productID);
+     $prostmt->execute();
+     $num = $prostmt->rowCount();
+    if($num != 0){
+        header('Location: product_read.php?action=productInStock');
+    } else {
         // delete query
         $query = "DELETE FROM products WHERE productID = ?";
         $stmt = $con->prepare($query);
@@ -17,9 +25,8 @@ try {
         } else {
             die('Unable to delete record.');
         }
-    
+    }
     $con->commit();
-
 
     // show error
 } catch (PDOException $exception) {
--- online_store/product_read.php
+++ online_store/product_read.php
@@ -26,6 +26,9 @@ if (!isset($_SESSION["cus_username"])) {
 
         $action = isset($_GET['action']) ? $_GET['action'] : "";
         // if it was redirected from delete.php
+        if ($action == 'productInStock') {
+            echo "<div class='alert alert-success'>Record could not deleted as this product in the order.</div>";
+        }
         
         if ($action == 'deleted') {
             echo "<div class='alert alert-success'>Record was deleted.</div>";
